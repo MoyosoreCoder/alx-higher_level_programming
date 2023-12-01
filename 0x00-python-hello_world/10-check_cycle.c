cf8cd4d5a9cf8bf587118fe895b8c3e8d4d2b6ba
Mariam <kelanimaryam1234@gmail.com>
2023-12-01 08:03:34 -0800

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - function in linked list that checks cycle
+ * @list: pointer to list
+ *
+ * Return: 1 onlist has cycle, 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
