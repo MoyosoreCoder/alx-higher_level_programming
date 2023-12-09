e6ae674798ab9f26317b751aafcb93139583a897
Mariam <kelanimaryam1234@gmail.com>
2023-12-08 20:21:15 -0800

    2-args.py updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if argc == 2:
             print("{} argument:".format(argc - 1))
         else:
-            print("{} arguments.".format(argc - 1))
+            print("{} arguments:".format(argc - 1))
         for n in range(1, argc):
             print('{}: {}'.format(n, argv[n]))
