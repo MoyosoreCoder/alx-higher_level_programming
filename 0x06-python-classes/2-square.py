a26ffd5d25e456adaa2500b3743c12c0cba280e2
Mariam <kelanimaryam1234@gmail.com>
2024-01-02 23:51:51 -0800

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" class Square that defines a square """
+
+
+class Square:
+    def __init__(self, size=0):
+        """built in method instantiated
+
+        Args:
+        @self: custom builtin already made by python
+        @size (int): size
+        """
+        self.__size = 0
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
