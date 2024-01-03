30b587f935dae172ba4778c240499c2a123a32f5
Mariam <kelanimaryam1234@gmail.com>
2024-01-02 23:05:59 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-""" an empty class named Square that defines a square """
+""" an empty class Square that defines a square """
 class Square:
+    """ pass doesn't do anything since we are not printing """
     pass
