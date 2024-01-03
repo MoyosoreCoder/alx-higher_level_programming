18b4915c188c648a740c3bcfdfd6508e79c697db
Mariam <kelanimaryam1234@gmail.com>
2024-01-02 23:20:33 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+""" class Square that defines a square by: (based on 0-square.py) """
+
+
 class Square:
+    """ built in method for passing the class objects """
     def __init__(self, size):
         self.__size = size
