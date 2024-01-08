cdfc1424bb862ad37d57e25325dade24d0078fa3
Mariam <kelanimaryam1234@gmail.com>
2024-01-08 06:10:29 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
-
-
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return [attr for attr in dir(obj)]
+    """ function that list attribute and methods """
+
+    for attr in [dir(obj)]:
+        """Return a list of an object's available attributes."""
+        return (attr)
