fa48139246f3c3e351ba5687134ad390a66c51ab
Mariam <kelanimaryam1234@gmail.com>
2024-01-16 08:22:18 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -5,6 +5,6 @@ say_my_name("John", "Smith")
 say_my_name("Walter", "White")
 say_my_name("Ayo", "")
 try:
-    say_my_name(12, "White")
+    say_my_name("A", "B", "C")
 except Exception as e:
     print(e)
