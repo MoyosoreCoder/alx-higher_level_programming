727142d62e680d05f87081b5ed3b59949e8dbe45
Mariam <kelanimaryam1234@gmail.com>
2024-03-12 21:35:42 -0700

    -occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
