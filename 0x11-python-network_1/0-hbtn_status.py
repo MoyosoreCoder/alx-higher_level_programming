dc66b43455e0dd6281bc00fb3b242bda7c4e0cb3
Mariam <kelanimaryam1234@gmail.com>
2024-03-31 04:10:23 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" 
-Module documentation for Python script to fetches https://alx-intranet.hbtn.io/status
-"""
+"""Python script to fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+
 def main():
-    """ Method that display the URL request of https://alx-intranet.hbtn.io/status """
+    """Method that display the URL request of https://alx-intranet"""
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url)  as response:
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print('Body Response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+
 if __name__ == "__main__":
     main()
