b902ff26a143696c151135979b2dc3fff681fa97
Mariam <kelanimaryam1234@gmail.com>
2024-04-01 06:04:26 -0700

    3-error_code.py file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """
-A Python script that takes in a URL, sends a request
-to the URL and displays the body of the response (decoded in utf-8).
+Takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)
 """
-import urllib.request
 import sys
+import urllib.request
 import urllib.error
-
 if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: {} <URL>".format(sys.argv[0]))
-            sys.exit(1)
-
-            url = sys.argv[1]
-            try:
-                with urllib.request.urlopen(url) as response:
-                    print(response.read().decode("utf-8"))
-            except urllib.error.HTTPError as e:
-                print("Error code: {}".format(e.code))
-            except urllib.error.URLError as e:
-                print("Failed to reach the server:", e.reason)
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e))
+    except urllib.error.URLError as e:
+        print("URL Error: {}".format(e))
+    except Exception as e:
+        print("Error: {}".format(e))
