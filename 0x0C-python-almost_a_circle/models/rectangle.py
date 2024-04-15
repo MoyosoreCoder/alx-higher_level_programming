0ff0703f4a6e7fca1e82110f866659eebe8194ee
Mariam <kelanimaryam1234@gmail.com>
2024-04-15 00:11:53 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ A python script with name Rectangle """
+    """ A python script with name Rectangle
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes Rectangle instance """
