5d4ec767eabbded893220502355d741ac25d1fc2
Mariam <kelanimaryam1234@gmail.com>
2024-04-15 03:57:00 -0700

    rectangle.py file added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,63 +1,79 @@
 #!/usr/bin/python3
-""" Python script that defines class Rectangle """
-from models.base import Base
+""" Python script that defines class Rectangle"""
 
 
 class Rectangle(Base):
-    """ A python script with name Rectangle"""
+    """Represent a class Rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initializes Rectangle instance
+        """Initialize a Base class with class rectangle.
+
         Args:
-            width(int): The width of the new Rectangle.
-            height(int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
+            width (int): The width in a Rectangle.
+            height (int): The height in a Rectangle.
+            x (int): The x variable in a Rectangle.
+            y (int): The y variable in a Rectangle.
+            id (int): The id parameter in the Rectangle.
         Raises:
             TypeError: If either of width or height is not an int.
             ValueError: If either of width or height <= 0.
             TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
-            """
-        super().__init__(id)
+            ValueError: If either of x or y <= 0.
+        """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
-        @property
-        def width(self):
-            """ Getter for width """
-            return self.__width
+    @property
+    def width(self):
+        """Getter of with in the Rectangle."""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """ Getter for height """
-            return self.__height
+    @property
+    def height(self):
+        """Getter of height in a Rectangle."""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """ Getter for x """
-            return self.__x
+    @property
+    def x(self):
+        """Getter of x in a Rectangle."""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """ Getter for y """
-            return self.__y
+    @property
+    def y(self):
+        """Getter of y in the Rectangle."""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
