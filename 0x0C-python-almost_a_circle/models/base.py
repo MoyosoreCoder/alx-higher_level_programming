a1879c625f6d3c4347314218c2f8aa9867633f7b
Mariam <kelanimaryam1234@gmail.com>
2024-04-24 03:39:22 -0700

    base.py file updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,19 +15,22 @@ class Base:
     """Method that initializes a new Base
     Args:
         id(int): The identity of the new Base.
+    """
 
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
+            self.id = Base.__no_bjects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries
-         Args:
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
             list_dictionaries (list): A list of dictionaries.
         """
-
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
