a8a3657437f3996c69d9a1f05b4c7a89723c6b05
Mariam <kelanimaryam1234@gmail.com>
2024-04-24 03:35:54 -0700

    base.py file updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,10 +5,17 @@ import json
 
 class Base:
     """ Base class for monitoring id attributes """
+    Private Class Attributes:
+        __nb_object(int): Number of instantiated Bases
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+    """Method that initializes a new Base
+    Args:
+        id(int): The identity of the new Base.
+
         if id is not None:
             self.id = id
         else:
@@ -16,7 +23,11 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries"""
+        """Return the JSON string representation of list_dictionaries
+         Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
